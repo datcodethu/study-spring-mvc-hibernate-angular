a2378fe68bd96dc39a3eb89e639616037b7fc9fc
dat <186383316+datcodethu@users.noreply.github.com>
2025-03-28 21:44:24 +0700

    upload views

--- spring_mvc_jsp_hibernate_2025/demo/src/main/java/com/example/demo/controller/ui/Test.java
+++ spring_mvc_jsp_hibernate_2025/demo/src/main/java/com/example/demo/controller/ui/Test.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller.ui;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class Test {
-
-    @RequestMapping("")
-    public String index() {
-        return "index";
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Tự động tìm index.html trong thư mục templates
     }
 }
